aa97f7c01271e2c730b8f40f2b396f36aa30ef6e
gonzalezjulianpro <julian@MacBook-Pro-de-Julian.local>
2023-11-24 18:13:04 -0300

    complemento jogo forca

--- forca.py
+++ forca.py
@@ -11,10 +11,12 @@ def jogar():
     #enquanto não enforcou E não acertou
     while(not enforcou and not acertou):
         chute = input("Qual letra?")
+        chute = chute.strip()
+
 
         index = 0
-        for letra in palavra_secraeta:
-            if(chute == letra):
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper()):
                 print("Encontrei a letra {} na posição {}".format(letra, index))
             index = index + 1
 
