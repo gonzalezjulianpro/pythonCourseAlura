f468c025ab5d6cb64289dd4834e24adcba921fec
gonzalezjulianpro <julian@MacBook-Pro-de-Julian.local>
2023-11-25 09:25:57 -0300

    Update jogo forca.

--- forca.py
+++ forca.py
@@ -4,6 +4,7 @@ def jogar():
     print("*********************************")
 
     palavra_secreta = "banana"
+    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
 
     enforcou = False
     acertou = False
@@ -17,10 +18,10 @@ def jogar():
         index = 0
         for letra in palavra_secreta:
             if(chute.upper() == letra.upper()):
-                print("Encontrei a letra {} na posição {}".format(letra, index))
+                letras_acertadas[index] = letra
             index = index + 1
 
-        print("jogando...")
+        print(letras_acertadas)
 
     print("Fim do jogo")
 
